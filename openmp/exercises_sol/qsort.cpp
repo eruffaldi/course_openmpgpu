1c087aad5429d72610b986ec2d3418499b65a02a
Emanuele Ruffaldi <emanuele.ruffaldi@gmail.com>
2016-09-29 18:36:08 +0200

    qsort

--- openmp/exercises_sol/qsort.cpp
+++ openmp/exercises_sol/qsort.cpp
@@ -5,6 +5,7 @@
  *
  * Take the following quicksort serial and paralellize it. Which other sorting algorithms can be parallelized
  */
+#include <omp.h>
 #include <iostream>
 #include <iterator>
 #include <vector>
@@ -27,6 +28,8 @@ bool check(I b, I e)
 	return true;
 }
 
+std::vector<int>::iterator bb; // hack for diagnostics
+
 // This is a non-stable quick sort
 // can we use OpenMP tasks for this?
 template <class I>
@@ -34,6 +37,7 @@ void qsort1(I b, I e)
 {
 	if(b == e)
 		return;
+	//std::cout << "omp_get_thread_num " << omp_get_thread_num() << " " << (b-bb) << "/" << (e-bb) << std::endl;
 	auto d = std::distance(b,e);
 	auto p = *std::next(b, d/2);
 	using W = decltype(p);
@@ -42,7 +46,11 @@ void qsort1(I b, I e)
 	//C++14 auto mid2 = std::partition(mid1,e,[p] (const auto & em) { return !(p < em); });
 	auto mid1 = std::partition(b,e,[p] (const W & em) { return em < p; });
 	auto mid2 = std::partition(mid1,e,[p] (const W & em) { return !(p < em); });
-	qsort1(b,mid1);
+	#pragma omp task if (d > 20) untied
+	{ 	
+		qsort1(b,mid1);
+	}
+	// continue in this (tail parallelism)
 	qsort1(mid2,e);
 }
 
@@ -52,16 +60,23 @@ int main(int argc, char * argv[])
 	std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> dis(1, 100);
-	std::vector<int> q(1000);
+	std::vector<int> q(10000000);
 
-	// QUESTION: can we parallelize this?
     for(int i = 0; i < q.size(); i++)
     	q[i] = dis(gen);
 
-	std::vector<int> q0 = q;
-	std::sort(q0.begin(),q0.end());
-	std::cout << "regular sort gives " << check(q0.begin(),q0.end()) << std::endl;
-	qsort1(q.begin(),q.end());
+	//std::vector<int> q0 = q;
+	//std::sort(q0.begin(),q0.end());
+	//std::cout << "regular sort gives " << check(q0.begin(),q0.end()) << std::endl;
+	#pragma omp parallel
+	{
+		#pragma omp single
+		{
+			bb = q.begin();
+			std::cout << "starting with " << omp_get_num_threads() << std::endl;
+			qsort1(q.begin(),q.end());
+		}
+	}
 	std::cout << "parallel sort gives " << check(q.begin(),q.end()) << std::endl;
 	return 0;
 }
